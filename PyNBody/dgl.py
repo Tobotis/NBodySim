08c7e1ccfe8899f8316e7ed67dd80180f3ceceb2
Tobotis <to.steinbrecher@gmail.com>
2021-12-17 16:56:31 +0100

    removed amuse

---
+++ PyNBody/dgl.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# Gravitationskonstante
+G = 1.0
+dT = 0.01
+# Ausgangsbedingungen für Körper 1
+X1 = np.array([0.0, 0.0, 0.0])
+X1_D = np.array([0.0, 0.0, 0.0])
+# Ausgangsbedingungen für Körper 2
+X2 = np.array([0.0, 0.0, 0.0])
+X2_D = np.array([0.0, 0.0, 0.0])
+# Ausgangsbedingungen für Körper 3
+X3 = np.array([0.0, 0.0, 0.0])
+X3_D = np.array([0.0, 0.0, 0.0])
+
+# Differentialgleichung
+def x_1_dd(x_1,x_1_d, t):
+    return G * ()
+
+# Lösung der Differential Gleichung
+def x_1(t):
+    x = X1
+    x_d = X1_D
+    for 
